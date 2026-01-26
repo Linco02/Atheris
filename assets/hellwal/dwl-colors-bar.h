4c184a37edabeeb8dfeed7976fd5ef2aa4fb10fc
Linco02 <dimahontar01@gmail.com>
2026-01-26 21:15:37 +0200

    п8

---
+++ assets/hellwal/dwl-colors-bar.h
@@ -0,0 +1,9 @@
+#define WALLPAPER "/home/nixlinco/Pictures/wallpaper/stat/wallhaven-l8x1pr.jpg"
+
+static const float rootcolor[]             = COLOR(0x04020eff);
+static uint32_t colors[][3]                = {
+	/*               fg          bg          border    */
+	[SchemeNorm] = { 0xf94cffff, 0x04020eff, 0x11083aff },
+	[SchemeSel]  = { 0xf94cffff, 0x53047eff, 0x2d024aff },
+	[SchemeUrg]  = { 0xf94cffff, 0x2d024aff, 0x53047eff },
+};
---
+++ assets/hellwal/dwl-colors.h
@@ -0,0 +1,6 @@
+#define WALLPAPER "/home/nixlinco/Pictures/wallpaper/stat/wallhaven-l8x1pr.jpg"
+
+static const float rootcolor[]             = COLOR(0x04020eff);
+static const float bordercolor[]           = COLOR(0x53047eff);
+static const float focuscolor[]            = COLOR(0x53047eff);
+static const float urgentcolor[]           = COLOR(0x2d024aff);
