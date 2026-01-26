aed7350cc880f0e543ffcbd9ff787a7c55c9f5d2
Linco02 <dimahontar01@gmail.com>
2026-01-26 13:57:38 +0200

    п2

---
+++ home-manager/users/aspire7/modules/thunar.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+      thunar-media-tags-plugin
+    ];
+  };
+}
--- nixos/modules/programs.nix
+++ nixos/modules/programs.nix
@@ -4,16 +4,5 @@
   programs = {
     fish.enable = true;
     firefox.enable = true;
-
-    thunar = {
-      enable = true;
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-volman
-        thunar-media-tags-plugin
-      ];
-    };
-
-
   };
 }
