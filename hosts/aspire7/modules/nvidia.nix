1f2382fffb5cf6a53f09381ed0ba8cd3ade9560e
Linco02 <dimahontar01@gmail.com>
2026-01-28 18:55:18 +0200

    Update nvidia.nix

--- hosts/aspire7/modules/nvidia.nix
+++ hosts/aspire7/modules/nvidia.nix
@@ -21,4 +21,12 @@
       };
     };
   };
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1"; # Дуже важливо для NVIDIA!
+  };
 }
