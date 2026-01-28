4c25d77f30354b1d4b5232d29dcdec5a7ba9bbc9
Linco02 <dimahontar01@gmail.com>
2026-01-28 15:38:06 +0200

    п4

--- home-manager/users/aspire7/modules/default.nix
+++ home-manager/users/aspire7/modules/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./git.nix
     # ./thunar.nix
     # ./vscode.nix
   ];
---
+++ home-manager/users/aspire7/modules/git.nix
@@ -0,0 +1,12 @@
+{
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name  = "Linco02";
+        email = "dimahontar01@gmail.com";
+      };
+      init.defaultBranch = "main";
+    };
+  };
+}
