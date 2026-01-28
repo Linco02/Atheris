a19f55ea8ebaa3c8742062aead2b04df8e86e5f5
Linco02 <dimahontar01@gmail.com>
2026-01-28 18:23:06 +0000

    Update nvidia.nix

--- hosts/aspire7/modules/nvidia.nix
+++ hosts/aspire7/modules/nvidia.nix
@@ -1,28 +1,23 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ...}:
 
 {
-  # 1. Спробуємо інше ядро (LTS зазвичай стабільніше для ноутбуків)
-  boot.kernelPackages = pkgs.linuxPackages_latest; # Або pkgs.linuxPackages_6_6 (LTS)
-
-  # 2. Додаємо параметри для виправлення ініціалізації AMD
-  boot.kernelParams = [ 
-    "amdgpu.noretry=0" 
-    "initcall_blacklist=simpledrm_platform_driver_init" # Вимикаємо конфліктний simpledrm
-    "nvidia_drm.modeset=1"
-    "nvidia_drm.fbdev=1"
-  ];
-
-  # 3. ПЕРЕКОНАЙТЕСЯ, що прошивка завантажується
-  hardware.enableAllFirmware = true;
-  hardware.cpu.amd.updateMicrocode = true;
-
-  # 4. Виправляємо порядок драйверів
+  # Увімкнення NVIDIA яй відеодрайвера
   services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
 
-  # 5. Обов'язково додаємо це для Hyprland + NVIDIA
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    # Змушуємо Hyprland бачити обидві карти, але AMD як основну для екрана
-    WLR_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      prime = {
+        offload.enable = true;
+        # offload.enableOffloadSetuidPrograms = true;
+
+        nvidiaBusId = "PCI:1:0:0";
+        amdgpuBusId = "PCI:5:0:0";
+      };
+    };
   };
 }
