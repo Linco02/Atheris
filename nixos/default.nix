21ae74de1636eedd136fa3b73aa1f3cbf3c4dad5
Linco02 <dimahontar01@gmail.com>
2026-01-28 18:38:14 +0200

    Update default.nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -19,7 +19,7 @@
   xdg.portal = {
     enable = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
+      # pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland
     ];
     config.common.default = "*";
