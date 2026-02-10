f312594f6467d2e7a31454696370fa4d812d6658
Linco02 <dimahontar01@gmail.com>
2026-02-10 18:20:21 +0200

    п2

--- home-manager/users/shark/modules/fish.nix
+++ home-manager/users/shark/modules/fish.nix
@@ -3,8 +3,8 @@
     enable = true;
     shellAliases = {
       dev = "nix develop ~/Atheris";
-      homeS = "home-manager switch --flake ~/Atheris#aspire7";
-      nixosS= "sudo nixos-rebuild switch --flake ~/Atheris#aspire7";
+      homeS = "home-manager switch --flake ~/Atheris#shark";
+      nixosS= "sudo nixos-rebuild switch --flake ~/Atheris#shark";
     };
   };
 }
