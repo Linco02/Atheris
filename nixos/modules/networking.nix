4da0a0a882a34c24217a9fc795ef7542a6464c05
Linco02 <dimahontar01@gmail.com>
2026-03-16 10:56:46 +0200

    п6

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -5,8 +5,8 @@
   networking = {
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 59100 27040 ];
-      allowedUDPPorts = [ 59100 59200 27040 ];
+      allowedTCPPorts = [ 59100 27036 24070 27037 ];
+      allowedUDPPorts = [ 59100 59200 24070 27031 27032 27033 27034 27035 27036 ];
     };
 
     hostName = hostname;
