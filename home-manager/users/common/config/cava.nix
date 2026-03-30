5b14ec39054fe3946c7e15abba031ef71bffa62e
Linco02 <dimahontar01@gmail.com>
2026-03-30 22:50:57 +0300

    fix cava

--- home-manager/users/common/config/cava.nix
+++ home-manager/users/common/config/cava.nix
@@ -1,18 +1,20 @@
-programs.cava = {
-  enable = true;
-  settings = {
-    general = {
-      framerate = 60;
-      bars = 20;
-    };
-    output = {
-      method = "raw";
-      raw_target = "/dev/stdout";
-      data_format = "ascii";
-      ascii_max_range = 100;
-    };
-    smoothing = {
-      noise_reduction = 77;
-    };
+{
+  programs.cava = {
+    enable = true;
+    # settings = {
+    #   general = {
+    #     framerate = 5;
+    #     bars = 20;
+    #   };
+    #   output = {
+    #     method = "raw";
+    #     raw_target = "/dev/stdout";
+    #     data_format = "ascii";
+    #     ascii_max_range = 100;
+    #   };
+    #   smoothing = {
+    #     noise_reduction = 77;
+    #   };
+    # };
   };
-};
+}
